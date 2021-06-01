10707e24555b51484de0b2ad6586d44a28b1b5e0
LongDirtyAnimAlf <LongDirtyAnimAlf@gmail.com>
2021-06-01 11:37:36 +0200

    Add general MQTT Climate client.

--- examples/Lazarus/AWS/unit1.pas
+++ examples/Lazarus/AWS/unit1.pas
@@ -36,6 +36,9 @@ implementation
 uses
   ssl_openssl_lib;
 
+const
+  LAB='General';
+
 {$R *.lfm}
 
 { TForm1 }
@@ -44,24 +47,46 @@ procedure TForm1.Button1Click(Sender: TObject);
 begin
   if (NOT IsSSLloaded) then InitSSLInterface;
   MyMQTTClient:=TMQTTClient.Create;
-  MyMQTTClient.ClientId:='SOHIT_Climate_Lab1';
+  MyMQTTClient.ClientId:='SOHIT_Climate_'+LAB;
   MyMQTTClient.Host:='a1imtw9krxr315-ats.iot.eu-west-1.amazonaws.com';
   MyMQTTClient.Port:=8883;
-  MyMQTTClient.SSLCertCAFile:=Application.Location+'Lab1Credentials\VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem';
-  MyMQTTClient.SSLCertificateFile:=Application.Location+'Lab1Credentials\324ddf45b4-certificate.pem.crt';
-  MyMQTTClient.SSLPrivateKeyFile:=Application.Location+'Lab1Credentials\324ddf45b4-private.pem.key';
+
+  if LAB='Lab1' then
+  begin
+    MyMQTTClient.SSLCertCAFile:=Application.Location+LAB+'Credentials\VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem';
+    MyMQTTClient.SSLCertificateFile:=Application.Location+LAB+'Credentials\324ddf45b4-certificate.pem.crt';
+    MyMQTTClient.SSLPrivateKeyFile:=Application.Location+LAB+'Credentials\324ddf45b4-private.pem.key';
+  end;
+
+  if LAB='Lab3' then
+  begin
+    MyMQTTClient.SSLCertCAFile:=Application.Location+LAB+'Credentials\VeriSign-Class 3-Public-Primary-Certification-Authority-G5.pem';
+    MyMQTTClient.SSLCertificateFile:=Application.Location+LAB+'Credentials\7423f4c7bd-certificate.pem.crt';
+    MyMQTTClient.SSLPrivateKeyFile:=Application.Location+LAB+'Credentials\7423f4c7bd-private.pem.key';
+  end;
+
+  if LAB='General' then
+  begin
+    MyMQTTClient.ClientId:='';
+    MyMQTTClient.SSLCertCAFile:=Application.Location+LAB+'Credentials\AmazonRootCA1.pem';
+    MyMQTTClient.SSLCertificateFile:=Application.Location+LAB+'Credentials\9c2f98cfdc-certificate.pem.crt';
+    MyMQTTClient.SSLPrivateKeyFile:=Application.Location+LAB+'Credentials\9c2f98cfdc-private.pem.key';
+  end;
+
   MyMQTTClient.SSLUse:=True;
   MyMQTTClient.AddTimeStamp:=True;
-  MyMQTTClient.Subscribe('$aws/things/'+MyMQTTClient.ClientId+'/shadow/update');
-  MyMQTTClient.Subscribe('$aws/things/'+MyMQTTClient.ClientId+'/shadow/update/accepted');
-  MyMQTTClient.Subscribe('$aws/things/'+MyMQTTClient.ClientId+'/shadow/update/rejected');
+  //MyMQTTClient.Subscribe('$aws/things/'+MyMQTTClient.ClientId+'/shadow/update');
+  //MyMQTTClient.Subscribe('$aws/things/'+MyMQTTClient.ClientId+'/shadow/update/accepted');
+  //MyMQTTClient.Subscribe('$aws/things/'+MyMQTTClient.ClientId+'/shadow/update/rejected');
+
+  MyMQTTClient.Subscribe('SOHITClimate');
   MyMQTTClient.OnConnect:=@ConnectionSuccess;
   MyMQTTClient.Start;
 end;
 
 procedure TForm1.Button2Click(Sender: TObject);
 begin
-  MyMQTTClient.Publish('$aws/things/'+MyMQTTClient.ClientId+'/shadow/update','hallo !!!');
+  //MyMQTTClient.Publish('$aws/things/'+MyMQTTClient.ClientId+'/shadow/update','hallo !!!');
 end;
 
 procedure TForm1.FormDestroy(Sender: TObject);
